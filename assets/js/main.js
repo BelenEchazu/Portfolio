c93622ca7885a16645c725f1d26701cc7ec9698f
UsuarioDeGitHub <belenechazu@outlook.com>
2023-04-07 13:39:34 +0100

    avances

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,64 @@ const tabs = document.querySelectorAll('[data-target]'),
 
         })
     })
+
+    /*====== MIXITUP FILTER PORTFOLIO ==============*/
+    let mixerPortfolio = mixitup('.work__container', {
+        selectors: {
+            target: '.work__card'
+        },
+        animation: {
+            duration: 300
+        }
+    });
+
+    /*===== Link Active Work=====*/
+    const linkWork = document.querySelectorAll('.work__item')
+
+    function activeWork() {
+        linkWork.forEach(l => l.classList.remove('active-work'))
+        this.classList.add('active-work')
+    }
+
+    linkWork.forEach(l=> l.addEventListener("click", activeWork))
+
+    /*======Work Popup====*/
+    document.addEventListener("click", (e)=>{
+        if(e.target.classList.contains("work__button")) {
+            togglePortfolioPopup();
+            portfolioItemDetails(e.target.parentElement);
+        }
+    })
+    function togglePortfolioPopup() {
+        document.querySelector(".portfolio__popup").classList.toggle("open")
+    }
+    document.querySelector(".portfolio__popup-close").addEventListener("click",togglePortfolioPopup)
+
+    function portfolioItemDetails(portfolioItem){
+        document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+        document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+        document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+    }
+
+    /*========== SERVICES MODAL ========*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modelBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close'),
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modelBtns.forEach((modelBtn, i) => {
+    modelBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove ('active-modal')
+        })
+    })
+})

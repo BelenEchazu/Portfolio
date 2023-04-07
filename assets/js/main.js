758e40e4a9b2742049f558199c7090e3c5c395e1
UsuarioDeGitHub <belenechazu@outlook.com>
2023-04-07 16:28:55 +0100

    avances

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,33 @@ inputs.forEach((input) => {
     input.addEventListener("focus", focusFunc)
     input.addEventListener("blur", blurFunc)
 })
+
+/*=============SCROLL SECTIONS ACTIVE LINK =============*/
+//get all sections that have an id defined
+const sections = document.querySelectorAll("section[id]");
+
+//add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter()
+{
+    //get current croll position
+    let scrollY = window.pageYOffset;
+    //Now we loop through sections to get height, top and ID values for each
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute("id");
+    
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+    {
+       document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add("active-link") 
+    }
+    else
+    {
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove("active-link")
+    }
+    })
+}
+
+/*==============SHOW CROLL UP ===============*/
